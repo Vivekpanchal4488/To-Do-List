98a6e1db3f61ac4627fc345c911f6584506f81bc
Vivek Panchal <vp443100@gmail.com>
2025-10-24 16:53:04 +0530

    Update script.js

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,15 +1,14 @@
-const inputbox = document.getElementById("input-box");
-const listcontainer = document.getElementById("list-container");
+const intbox = document.getentById("input-box");
+const listcntainer = document.getentById("list-container");
 
 function addTask(){  // button add if we click that operation happens
-    if(inputbox.value === ''){ // if input is empty then its displays this
+    if(inputbox.v=== ''){ // if input is empty then its displays this
         alert("You must write task!");
     }
     else{ // if input not empty then
         let li = document.createElement("li");  // new li variable creates 
-        li.innerHTML = inputbox.value; // inside li all that of input is stored value
-        listcontainer.appendChild(li); // now li append and display in liostcontainer
-        let span = document.createElement("span")
+        li.innerHTML = inpow append and display in liostcontainer
+        let span = ent.createElement("span")
         span.innerHTML = "\u00d7";
         li.appendChild(span)
     }
@@ -18,22 +17,13 @@ function addTask(){  // button add if we click that operation happens
 }
 
 
-listcontainer.addEventListener("click", function(e){ // for clicking
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
 
 function saveData(){
-    localStorage.setItem("data", listcontainer.innerHTML);
+    localStorage.setItem("dnnerHTML);
 }
 
 function showTask(){
     listcontainer.innerHTML = localStorage.getItem("data");
 }
+
 showTask();
